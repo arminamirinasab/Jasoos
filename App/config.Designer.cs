ef1ba500bab1f2448b28db8d1e661eda8d971b3f
arminamirinasab <arminamirinasab@gmail.com>
2024-01-29 21:53:36 +0330

    Complete Menu UI Design

--- App/config.Designer.cs
+++ App/config.Designer.cs
@@ -29,21 +29,158 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(config));
+            this.startBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.farmerCount = new System.Windows.Forms.ComboBox();
+            this.spyCount = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.startBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.startBtn.FlatAppearance.BorderSize = 0;
+            this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startBtn.Font = new System.Drawing.Font("Mikhak-FD Medium Line Double Do", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startBtn.ForeColor = System.Drawing.Color.White;
+            this.startBtn.Location = new System.Drawing.Point(0, 372);
+            this.startBtn.Margin = new System.Windows.Forms.Padding(5);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(484, 53);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "شروع بازی";
+            this.startBtn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::App.Properties.Resources.farmer;
+            this.pictureBox1.Location = new System.Drawing.Point(63, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::App.Properties.Resources.spy;
+            this.pictureBox2.Location = new System.Drawing.Point(266, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 170);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mikhak-FD ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(289, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "تعداد جاسوسان";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mikhak-FD ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(90, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "تعداد کشاورزان";
+            // 
+            // farmerCount
+            // 
+            this.farmerCount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.farmerCount.Font = new System.Drawing.Font("Mikhak Line Double Dots", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.farmerCount.FormattingEnabled = true;
+            this.farmerCount.ItemHeight = 23;
+            this.farmerCount.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.farmerCount.Location = new System.Drawing.Point(63, 286);
+            this.farmerCount.Name = "farmerCount";
+            this.farmerCount.Size = new System.Drawing.Size(155, 31);
+            this.farmerCount.TabIndex = 7;
+            // 
+            // spyCount
+            // 
+            this.spyCount.DisplayMember = "3";
+            this.spyCount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.spyCount.Font = new System.Drawing.Font("Mikhak Line Double Dots", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.spyCount.FormattingEnabled = true;
+            this.spyCount.ItemHeight = 23;
+            this.spyCount.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.spyCount.Location = new System.Drawing.Point(266, 286);
+            this.spyCount.Name = "spyCount";
+            this.spyCount.Size = new System.Drawing.Size(155, 31);
+            this.spyCount.TabIndex = 9;
             // 
             // config
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(236)))), ((int)(((byte)(246)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 425);
+            this.Controls.Add(this.spyCount);
+            this.Controls.Add(this.farmerCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startBtn);
+            this.Font = new System.Drawing.Font("Mikhak-FD Medium Line Double Do", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "config";
             this.Text = "Jasoos";
+            this.Load += new System.EventHandler(this.config_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button startBtn;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private ComboBox farmerCount;
+        private ComboBox spyCount;
     }
 }
--- App/config.cs
+++ App/config.cs
@@ -1,10 +1,17 @@
-namespace App
+﻿namespace App
 {
     public partial class config : Form
     {
+
         public config()
         {
             InitializeComponent();
         }
+
+        private void config_Load(object sender, EventArgs e)
+        {
+           farmerCount.SelectedIndex = spyCount.SelectedIndex = 0;
+        }
+
     }
 }
